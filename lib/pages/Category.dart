2cfe047e26ab9a250db51f7a73739ff4e07e780f
天弃之子 <mumu.918@qq.com>
2022-08-19 14:01:59 +0800

    -

--- lib/pages/Category.dart
+++ lib/pages/Category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Category extends StatefulWidget {
   const Category({Key? key}) : super(key: key);
@@ -8,10 +9,85 @@ class Category extends StatefulWidget {
 }
 
 class _CategoryState extends State<Category> {
+  int _selectIndex = 0;
+  
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('我是分类'),
+    return Row(
+      children: [
+        Container(
+            width: ScreenUtil().setWidth(200),
+            height: double.infinity,
+            child: ListView.builder(
+              itemBuilder: ((context, index) {
+                return Column(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          setState(() {
+                            _selectIndex = index;
+                          });
+                        },
+                        child: Container(
+                            width: double.infinity,
+                            height: ScreenUtil().setHeight(50),
+                            child: Center(
+                              child: Text(
+                                'is $index',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: _selectIndex == index
+                                      ? Colors.pink
+                                      : Colors.black87,
+                                ),
+                              ),
+                            ))),
+                    Divider(
+                      color:
+                          _selectIndex == index ? Colors.pink : Colors.black87,
+                    )
+                  ],
+                );
+              }),
+              itemCount: 18,
+            )),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+              height: double.infinity,
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1 / 1.2,
+                    crossAxisSpacing: ScreenUtil().setWidth(10),
+                    mainAxisSpacing: ScreenUtil().setWidth(10),
+                  ),
+                  itemBuilder: ((context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(ScreenUtil().setWidth(10)),
+                      child: Column(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Image.network(
+                              'https://img0.baidu.com/it/u=3085690425,3847123260&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1660842000&t=5c5505813673d91db9fda6df725d3aee',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                width: double.infinity,
+                                color: Colors.pink[200],
+                                child: Text('111'),
+                              ))
+                        ],
+                      ),
+                    );
+                  }))),
+        ),
+      ],
     );
   }
 }
